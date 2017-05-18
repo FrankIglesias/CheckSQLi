293a5d0a9526cd94bbbe97e99c2dbc8486b7bd09
FrankIglesias <frank.beatle@gmail.com>
2017-05-18 19:36:02 -0300

    arreglo error de ortografia

--- Script-sqli.py
+++ Script-sqli.py
@@ -7,12 +7,10 @@ for carg in sys.argv:
             argnum = sys.argv.index(carg)
             argnum+=1
             fullurl = sys.argv[argnum] #Cargo el argumento a la url
-             """Agrego a la url la sentencia and persona enrique
-             esto me deberia dar un error de sintaxis o al menos decirme que no existe la columna"""
             resp = urllib.request.urlopen(fullurl + "%20and%20persona%20=%20%27enrique%27")
             body = resp.read()
             fullbody= body.decode('utf-8')
             if  "SQL syntax" in fullbody or "column" in fullbody: #Si encuentra esto en la respuesta a la url
-                print ("La pagina es bulnerable a SQL injection")
+                print ("La pagina es vulnerable a SQL injection")
             else:
                 print("La pagina es segura de SQL injection")
